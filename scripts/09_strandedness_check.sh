444e51622db30c68b395059045abea9bc7739ada
nourhan947 <nourhansaad1999n@gmail.com>
2026-09-23 08:10:20 +0300

    Update 09_strandedness_check.sh

--- scripts/09_strandedness_check.sh
+++ scripts/09_strandedness_check.sh
@@ -12,4 +12,4 @@ This is PairEnd Data
 Fraction of reads failed to determine: 0.1401
 Fraction of reads explained by "1++,1--,2+-,2-+": 0.4272
 Fraction of reads explained by "1+-,1-+,2++,2--": 0.4327
- read_distribution.py -i /media/sf_course_data/alignments/SRR15852423_Aligned.sortedByCoord.out.bam -r ~/rnaseq_project/Homo_sapiens.GRCh38.116.bed
+
